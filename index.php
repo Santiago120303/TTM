4f4c1ca2a1f7d3e16675d645a64c2ff585ae6f20
Santiago120303 <112451290+Santiago120303@users.noreply.github.com>
2023-10-18 09:43:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+<head>
+
+	<!-- ** Basic Page Needs ** -->
+	<meta charset="utf-8">
+	<title>Home Tomando tu mano</title>
+
+	<!-- ** Mobile Specific Metas ** -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Agency HTML Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+	<meta name="author" content="Themefisher">
+	<meta name="generator" content="Themefisher Classified Marketplace Template v1.0">
+
+	<!-- theme meta -->
+	<meta name="theme-name" content="classimax" />
+
+	<!-- favicon -->
+	<link href="Views/images/favicon.png" rel="shortcut icon">
+
+	<!-- 
+  Essential stylesheets
+  =====================================-->
+	<link href="Views/plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="Views/plugins/bootstrap/bootstrap-slider.css" rel="stylesheet">
+	<link href="Views/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="Views/plugins/slick/slick.css" rel="stylesheet">
+	<link href="Views/plugins/slick/slick-theme.css" rel="stylesheet">
+	<link href="Views/plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+
+	<link href="Views/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="body-wrapper">
+
+
+	<header id="index">
+		<div class="container-fluid header-container">
+			<div class="row">
+				<div class="col-md-12">
+					<nav class="navbar navbar-expand-lg navbar-light navigation">
+						<a class="navbar-brand" href="index.php">
+							<img src="Views/images/logo.png" alt="Logo TTM" />
+						</a>
+						<button class="navbar-toggler m0a" type="button" data-toggle="collapse"
+							data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+							aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav ml-auto main-nav">
+								<li class="nav-item">
+									<a class="nav-link" href="index.php">Inicio</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link nav-item" href="Views/clientSite/fundaciones.php">Fundaciones</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="Views/clientSite/mascotas.php">Mascotas</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="Views/clientSite/events.php">Eventos</a>
+								</li>
+								<!-- <li class="nav-item">
+									<a class="nav-link" href="Views/clientSite/donation.php">Donación</a>
+								</li> -->
+								<li class="nav-item">
+									<a class="nav-link" href="Views/clientSite/aboutUs.php">Acerca de Nosotros</a>
+								</li>
+							</ul>
+							<ul class="navbar-nav ml-auto mt-10">
+								<li class="nav-item">
+									<a class="nav-link login-button" href="Views/clientSite/login.php">Ingresar</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link login-button nav-button" href="Views/clientSite/register.php">Registrarse</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!--===============================
+=            Hero Area            =
+================================-->
+
+	<section class="hero-area bg-1 text-center overly">
+		<!-- Container Start -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<!-- Header Contetnt -->
+					<div class="content-block">
+						<h1 class="titulos">Tomando tu Mano</h1>
+						<p>"Tomando Tu Mano" representa un apasionante proyecto de desarrollo de software que tiene como
+							objetivo brindar un merecido reconocimiento a las fundaciones de mascotas con sede en Bogotá. Nuestra
+							meta es permitir que la ciudadanía identifique y respalde de manera más efectiva a aquellas
+							entidades que requieren un mayor apoyo y atención.</p>
+	</section>
+
+
+	<!--===========================================
+=            Popular deals section            =
+============================================-->
+
+	<section class="popular-deals section bg-gray">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-title">
+						<h2>Nuestros servicios</h2>
+						<p>A continuación, te presentamos una selección de los servicios que ponemos a tu
+							disposición.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<!-- offer 01 -->
+				<div class="col-lg-12">
+					<div class="trending-ads-slide">
+
+
+						<div class="col-sm-12 col-lg-4">
+							<div class="product-item bg-light">
+								<div class="card">
+									<div class="thumb-content">
+										<a href="Views/clientSite/fundaciones.php">
+											<img class="card-img-top img-fluid"
+												src="Views/images/products/products-1.jpg" alt="Card image cap">
+										</a>
+									</div>
+									<div class="card-body">
+										<h4 class="card-title"><a href="Views/clientSite/fundaciones.php">Fundaciones</a></h4>
+										<p class="card-text">En nuestro sitio, tendrás acceso a una amplia gama de
+											fundaciones de animales ubicadas en Bogotá. En cada perfil, encontrarás
+											información esencial detallada sobre cada una de estas organizaciones.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+						<!-- <div class="col-sm-12 col-lg-4">
+							<div class="product-item bg-light">
+								<div class="card">
+									<div class="thumb-content">
+										<a href="single.php">
+											<img class="card-img-top img-fluid"
+												src="Views/images/products/products-2.jpg" alt="Card image cap">
+										</a>
+									</div>
+									<div class="card-body">
+										<h4 class="card-title"><a href="single.php">Donaciones</a></h4>
+										<p class="card-text"> A través de nuestro sitio, tendrás la oportunidad única de
+											brindar tu apoyo vital a estas organizaciones, contribuyendo directamente a
+											fortalecer y potenciar sus funciones esenciales en beneficio de los
+											animales.</p>
+									</div>
+								</div>
+							</div>
+						</div> -->
+
+
+						<div class="col-sm-12 col-lg-4">
+							<div class="product-item bg-light">
+								<div class="card">
+									<div class="thumb-content">
+										<a href="Views/clientSite/mascotas.php">
+											<img class="card-img-top img-fluid"
+												src="Views/images/products/products-3.png" alt="Card image cap">
+										</a>
+									</div>
+									<div class="card-body">
+										<h4 class="card-title"><a href="Views/clientSite/mascotas.php">Mascotas</a></h4>
+										<p class="card-text">A través de nuestra plataforma, te brindamos la oportunidad
+											de descubrir una amplia diversidad de adorables animales que están en busca
+											de un hogar amoroso. ¡No pierdas tiempo, únete a nosotros y abre tus brazos
+											a la adopción!</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="col-sm-12 col-lg-4">
+							<div class="product-item bg-light">
+								<div class="card">
+									<div class="thumb-content">
+										<a href="Views/clientSite/events.php">
+											<img class="card-img-top img-fluid"
+												src="Views/images/products/products-4.png" alt="Card image cap">
+										</a>
+									</div>
+									<div class="card-body">
+										<h4 class="card-title"><a href="Views/clientSite/events.php">Eventos</a></h4>
+										<p class="card-text">Explora nuestra plataforma y sumérgete en una amplia
+											variedad de eventos respaldados por diversas fundaciones. Regístrate de
+											manera sencilla y lleva un registro de tus participaciones. Tu compromiso
+											hacia un cambio positivo.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!--====================================
+=            Call to Action            =
+=====================================-->
+
+	<section class="call-to-action overly bg-3 section-sm">
+		<!-- Container Start -->
+		<div class="container">
+			<div class="row justify-content-md-center text-center">
+				<div class="col-md-8">
+					<div class="content-holder">
+						<h2>¡Únete a la acción y sé parte del cambio! Desde donaciones que salvan vidas hasta adopciones
+							que llenan de amor, cada acción cuenta. Regístrate hoy y contribuye al impacto positivo.
+							¡Actúa ahora!</h2>
+						<ul class="list-inline mt-30">
+							<li class="list-inline-item"><a class="btn btn-main"
+									href="Views/clientSite/login.php">Ingresar</a></li>
+							<li class="list-inline-item"><a class="btn btn-light"
+									href="Views/clientSite/register.php">Registrarse</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Container End -->
+	</section>
+
+	<!--============================
+=            Footer            =
+=============================-->
+	<!-- Footer Bottom -->
+	<footer class="footer-bottom">
+		<!-- Container Start -->
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-4 text-center text-lg-left mb-3 mb-lg-0">
+					<!-- logo -->
+					<a class="navbar-brand" href="index.php">
+						<img class="m0a" src="Views/images/logo-footer.png" alt="Logo TTM">
+					</a>
+				</div>
+				<div class="col-lg-4 text-center mb-3 mb-lg-0">
+					<!-- Copyright -->
+					<div class="copyright">
+						<p>Copyright &copy;
+							<script>
+								var CurrentYear = new Date().getFullYear()
+								document.write(CurrentYear)
+							</script> Tomando Tu Mano. Todos los derechos reservados.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<!-- Social Icons -->
+					<ul class="social-media-icons text-center text-lg-right">
+						<li><a class="fa fa-facebook" href="https://www.facebook.com/themefisher"></a></li>
+						<li><a class="fa fa-twitter" href="https://www.twitter.com/themefisher"></a></li>
+						<li><a class="fa fa-pinterest-p" href="https://www.pinterest.com/themefisher"></a>
+						</li>
+						<li><a class="fa fa-github-alt" href="https://www.github.com/themefisher"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- Container End -->
+		<!-- To Top -->
+		<div class="scroll-top-to">
+			<i class="fa fa-angle-up"></i>
+		</div>
+	</footer>
+
+	<!-- 
+Essential Scripts
+=====================================-->
+	<script src="Views/plugins/jquery/jquery.min.js"></script>
+	<script src="Views/plugins/bootstrap/popper.min.js"></script>
+	<script src="Views/plugins/bootstrap/bootstrap.min.js"></script>
+	<script src="Views/plugins/bootstrap/bootstrap-slider.js"></script>
+	<script src="Views/plugins/tether/js/tether.min.js"></script>
+	<script src="Views/plugins/raty/jquery.raty-fa.js"></script>
+	<script src="Views/plugins/slick/slick.min.js"></script>
+	<script src="Views/plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+	<!-- google map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+	<script src="Views/plugins/google-map/map.js" defer></script>
+
+	<script src="Views/js/script.js"></script>
+
+</body>
+
+</html>
