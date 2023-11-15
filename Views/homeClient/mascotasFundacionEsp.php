9f53b287c4812579d47c9c0bfd885cc90a9f0654
no-desconfigure-el-git <cristian.c.lopez.m@gmail.com>
2023-11-15 11:38:44 -0500

    Cambios merge

---
+++ Views/homeClient/mascotasFundacionEsp.php
@@ -0,0 +1,153 @@
+<?php
+  require_once("../../Models/conexion.php");
+  require_once("../../Models/consultas.php");
+  require_once("../../Models/seguridadClient.php");
+  require_once("../../Controllers/mostrarInfoClient.php");
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<!-- ** Basic Page Needs ** -->
+	<meta charset="utf-8">
+	<title>Mascotas - TTM</title>
+
+	<!-- ** Mobile Specific Metas ** -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Agency HTML Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+	<meta name="author" content="Themefisher">
+	<meta name="generator" content="Themefisher Classified Marketplace Template v1.0">
+
+	<!-- favicon -->
+	<link href="../images/favicon.png" rel="shortcut icon">
+
+	<!-- 
+  Essential stylesheets
+  =====================================-->
+	<link href="../plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="../plugins/bootstrap/bootstrap-slider.css" rel="stylesheet">
+	<link href="../plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../plugins/slick/slick.css" rel="stylesheet">
+	<link href="../plugins/slick/slick-theme.css" rel="stylesheet">
+	<link href="../plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+
+	<link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="body-wrapper">
+
+	<?php
+		include("nav-cliente.php");
+	?>
+
+	<section class="section-sm minhe">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="search-result bg-gray text-center">
+						<h2>Mascotas</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-md-4">
+					<!-- Search Widget -->
+					<div class="widget search p-0">
+						<div class="input-group">
+							<input type="text" class="form-control" id="expire"
+								placeholder="Buscar...">
+							<span class="input-group-addon"><i class="fa fa-search px-3"></i></span>
+						</div>
+					</div>
+					<div class="category-sidebar">
+						<div class="widget product-shorting">
+							<h4 class="widget-header">Filtrar Mascotas</h4>
+							<div class="form-check">
+								<label class="form-check-label">
+									<input class="form-check-input" type="checkbox" value="">
+									Gatos
+								</label>
+							</div>
+							<div class="form-check">
+								<label class="form-check-label">
+									<input class="form-check-input" type="checkbox" value="">
+									Perros
+								</label>
+							</div>
+							<div class="form-check">
+								<label class="form-check-label">
+									<input class="form-check-input" type="checkbox" value="">
+									Aves
+								</label>
+							</div>
+							<div class="form-check">
+								<label class="form-check-label">
+									<input class="form-check-input" type="checkbox" value="">
+									Hogar Provisional
+								</label>
+							</div>
+						</div>
+
+					</div>
+				</div>
+				
+				<div class="col-lg-9 col-md-8">
+					<div class="product-grid-list">
+						<div class="row mt-30">
+							<div class="col-lg-4 col-md-6">
+								<!-- product card -->
+								<div class="product-item bg-light">
+									<div class="card">
+										<div class="thumb-content">
+											<!-- <div class="price">$200</div> -->
+											<a class="bg-mascota" href="mascota.php">
+												<img class="card-img-top img-fluid"
+													src="../../Uploads/mascotas/mascota1.png"
+													alt="Card image cap">
+											</a>
+										</div>
+										<div class="card-body">
+											<h4 class="card-title"><a href="mascota.php">Tom</a></h4>				
+											<div class="product-ratings">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+	</section>
+	<!--============================
+=            Footer            =
+=============================-->
+
+	<?php
+		include("footer-include.php")
+	?>
+
+	<!-- 
+Essential Scripts
+=====================================-->
+	<script src="../plugins/jquery/jquery.min.js"></script>
+	<script src="../plugins/bootstrap/popper.min.js"></script>
+	<script src="../plugins/bootstrap/bootstrap.min.js"></script>
+	<script src="../plugins/bootstrap/bootstrap-slider.js"></script>
+	<script src="../plugins/tether/js/tether.min.js"></script>
+	<script src="../plugins/raty/jquery.raty-fa.js"></script>
+	<script src="../plugins/slick/slick.min.js"></script>
+	<script src="../plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+	<!-- google map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+	<script src="../plugins/google-map/map.js" defer></script>
+
+	<script src="../js/script.js"></script>
+
+</body>
+
+</html>
---
+++ Views/homefundacion/fundaciones.php
@@ -0,0 +1,153 @@
+<?php
+  require_once("../../Models/conexion.php");
+  require_once("../../Models/consultas.php");
+  require_once("../../Models/seguridadFundacion.php");
+  require_once("../../Controllers/mostrarInfoFundacion.php");
+?>
+
+<!DOCTYPE html>
+
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<html lang="en">
+
+<head>
+
+	<!-- ** Basic Page Needs ** -->
+	<meta charset="utf-8">
+	<title>Fundaciones</title>
+
+	<!-- ** Mobile Specific Metas ** -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Agency HTML Template">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+	<meta name="author" content="Themefisher">
+	<meta name="generator" content="Themefisher Classified Marketplace Template v1.0">
+
+	<!-- favicon -->
+	<link href="../images/favicon.png" rel="shortcut icon">
+
+	<!-- 
+  Essential stylesheets
+  =====================================-->
+	<link href="../plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="../plugins/bootstrap/bootstrap-slider.css" rel="stylesheet">
+	<link href="../plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../plugins/slick/slick.css" rel="stylesheet">
+	<link href="../plugins/slick/slick-theme.css" rel="stylesheet">
+	<link href="../plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+
+	<link href="../css/style.css" rel="stylesheet">
+
+	
+    <!-- Estilos propios del documento -->
+    <style>
+         .list {
+        max-height: 200px !important;
+        overflow:hidden !important;
+        overflow-y: scroll !important;
+        }
+	   input::placeholder{
+		font-family: 'Montserrat', sans-serif;
+	   }
+    </style>
+
+</head>
+
+<body class="body-wrapper">
+
+	<?php
+		include("nav-fundacion.php");
+	?>
+
+<section class="section-sm minhe">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="search-result bg-gray text-center">
+					<h2>Fundaciones</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-3 col-md-4">
+				<!--Buscador-->
+				<form method="get">
+				<input class="mt-3 nice-select form-control w-100 text-center" type="text" name="fundacionNombre" placeholder="Fundacion...">
+				<select name="localidad" id="" class="form-control mt-3 nice-select form-control w-100 text-center"  >
+					<option value="">Localidad</option>
+					<option value="1">Antonio Nariño</option>
+					<option value="2">Barrio Unidos</option>
+					<option value="3">Bosa</option>
+					<option value="4">Chapinero</option>
+					<option value="5">Ciudad Bolívar</option>
+					<option value="6">Engativá</option>
+					<option value="7">Fontibón</option>
+					<option value="8">Kennedy</option>
+					<option value="9">La Candelaria</option>
+					<option value="10">Los Mártires</option>
+					<option value="11">Puente Aranda</option>
+					<option value="12">Rafael Uribe Uribe</option>
+					<option value="13">San Cristóbal</option>
+					<option value="14">Santa Fe</option>
+					<option value="15">Suba</option>
+					<option value="16">Sumapaz</option>
+					<option value="17">Teusaquillo</option>
+					<option value="18">Tunjuelito</option>
+					<option value="19">Usaquén</option>
+					<option value="20">Usme</option>
+				</select>
+				<input class="mt-3 btn btn-login" type="submit" value="Buscar">
+				</form>
+			</div>
+			<div class="col-lg-9 col-md-8">
+				<div class="product-grid-list">
+					<div class="row mt-30">
+						<?php
+							if (isset($_GET['fundacionNombre']) || isset ($_GET['localidad'])) {
+								buscarNombreFundacion($_GET['fundacionNombre'], $_GET['localidad']);
+							}else{
+								cargarFundaciones1();
+							}
+								
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!--============================
+=            Footer            =
+=============================-->
+
+	<?php
+		include("footer-include.php")
+	?>
+
+	<!-- 
+Essential Scripts
+=====================================-->
+	<script src="../plugins/jquery/jquery.min.js"></script>
+	<script src="../plugins/bootstrap/popper.min.js"></script>
+	<script src="../plugins/bootstrap/bootstrap.min.js"></script>
+	<script src="../plugins/bootstrap/bootstrap-slider.js"></script>
+	<script src="../plugins/tether/js/tether.min.js"></script>
+	<script src="../plugins/raty/jquery.raty-fa.js"></script>
+	<script src="../plugins/slick/slick.min.js"></script>
+	<script src="../plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+	<!-- google map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+	<script src="../plugins/google-map/map.js" defer></script>
+
+	<script src="../js/script.js"></script>
+
+</body>
+
+</html>
